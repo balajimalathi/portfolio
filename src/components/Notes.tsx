ca686e90ee7620695630b91c325dc214b6652f69
Balaji <balaji.m@splenta.com>
2025-03-28 19:38:27 +0530

    Refactoring

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,6 +1,5 @@
 import type { CollectionEntry } from "astro:content"
 import { createEffect, createSignal, For } from "solid-js"
-import ArrowCard from "@components/ArrowCard"
 import { cn } from "@lib/utils"
 import NotesCard from "./NotesCard"
 
